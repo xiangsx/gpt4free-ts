a45e00bd3d0f7765422b3468653e1ac72439ca43
xiang <1984871009@qq.com>
2023-05-05 21:18:40 +0800

    feat: add run success log

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import Koa from 'koa';
 import Router from 'koa-router'
 import bodyParser from 'koa-bodyparser';
 import {ChatModelFactory, Model} from "./model";
+import type {Middleware} from "koa";
 
 const app = new Koa();
 const router = new Router();
@@ -50,4 +51,6 @@ router.get('/ask/stream', async (ctx) => {
 
 app.use(router.routes());
 
-app.listen(3000);
+app.listen(3000,()=>{
+    console.log("Now listening: 127.0.0.1:3000");
+});
