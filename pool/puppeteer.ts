4e48f9f2fbd044ae4ef0a29521c24a8a7f29b1db
xiang <1984871009@qq.com>
2023-06-06 12:38:30 +0800

    feat(forefront): support account cache

--- pool/puppeteer.ts
+++ pool/puppeteer.ts
@@ -5,52 +5,57 @@ import run from "node:test";
 const runPath = path.join(__dirname, 'run');
 
 export interface PageInfo<T> {
-    id: number;
+    id: string;
     ready: boolean;
     page?: Page;
     data?: T;
 }
 
-type PrepareFunc<T> = (browser: Browser) => Promise<Page>
+type PrepareFunc<T> = (id: string, browser: Browser) => Promise<[Page, T]>
 
 export class BrowserPool<T> {
     private readonly pool: PageInfo<T>[] = [];
     private readonly size: number;
     private readonly prepare: PrepareFunc<T>
 
-    constructor(size: number, prepare: PrepareFunc<T>,) {
+    constructor(size: number, initialIDs: string[], prepare: PrepareFunc<T>) {
         this.size = size
         this.prepare = prepare;
-        this.init();
+        this.init(initialIDs);
     }
 
-    init() {
+    init(initialIDs: string[]) {
         for (let i = 0; i < this.size; i++) {
+            const id = initialIDs[i];
             const info: PageInfo<T> = {
-                id: i,
+                id,
                 ready: false,
             }
-            this.initOne().then(page => {
+            this.initOne(id).then(([page, data]) => {
                 info.page = page;
+                info.data = data;
                 info.ready = true;
-            }).catch(e=>{
+            }).catch(e => {
                 console.error(e);
             })
             this.pool.push(info)
         }
     }
 
-    async initOne(): Promise<Page> {
+    async initOne(id: string): Promise<[Page, T]> {
         const options: PuppeteerLaunchOptions = {
             headless: process.env.DEBUG === "1" ? false : 'new',
             args: ['--no-sandbox'],
         };
-        const browser = await puppeteer.launch(options)
-        return this.prepare(browser)
+        if (id) {
+            options.userDataDir = `run/${id}`;
+        }
+        const browser = await puppeteer.launch(options);
+        return this.prepare(id, browser)
     }
 
     //@ts-ignore
-    get(): [page: Page | undefined, data: T | undefined, done: (data: T) => void, destroy: () => void] {
+    get(): [page: Page | undefined, data: T | undefined, done: (data: T) => void, destroy: (newID: string) => void] {
         for (const item of this.pool) {
             if (item.ready) {
                 item.ready = false;
@@ -61,10 +66,11 @@ export class BrowserPool<T> {
                         item.ready = true
                         item.data = data;
                     },
-                    () => {
+                    (newID: string) => {
                         item.page?.close();
-                        this.initOne().then((page) => {
+                        this.initOne(newID).then(([page, data]) => {
                             item.page = page
+                            item.data = data;
                             item.ready = true;
                         })
                     }
