645ac22e952e3107ed5948e021234cfedc38ee3f
fhtwl <15927027721@163.com>
2023-06-07 15:37:34 +0800

    delete dist

--- dist/src/model/base.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Chat = void 0;
-class Chat {
-    constructor(options) {
-        this.options = options;
-    }
-}
-exports.Chat = Chat;
