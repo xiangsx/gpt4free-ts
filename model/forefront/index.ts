44648b35bd604bf18a55393dc65b715efa951a30
xiang <1984871009@qq.com>
2023-06-09 10:46:44 +0800

    fix(forefront): done text

--- model/forefront/index.ts
+++ model/forefront/index.ts
@@ -58,7 +58,7 @@ class AccountPool {
 
     public get(): Account {
         const now = moment();
-        const minInterval = 3000 * 60 * 60 + 10 * 60;// 3hour + 10min
+        const minInterval = 3 * 60 * 60 + 10 * 60;// 3hour + 10min
         for (const item of this.pool) {
             if (now.unix() - moment(item.last_use_time).unix() > minInterval) {
                 console.log(`find old login account:`, item);
