a74d410db8ef380e81f94c4cac8fa90a4d28ef0f
xiang <1984871009@qq.com>
2023-06-13 20:54:45 +0800

    feat: add site

---
+++ model/chatdemo/index.ts
@@ -0,0 +1,97 @@
+import {Chat, ChatOptions, ChatRequest, ChatResponse, ModelType} from "../base";
+import {AxiosInstance, AxiosRequestConfig, CreateAxiosDefaults} from "axios";
+import {CreateAxiosProxy} from "../../utils/proxyAgent";
+import es from "event-stream";
+import {ErrorData, Event, EventStream, MessageData, parseJSON} from "../../utils";
+import {randomUUID} from "crypto";
+import {v4} from "uuid";
+import moment from "moment";
+
+interface RealReq {
+    question: string;
+    chat_id: string;
+    timestamp: number;
+}
+
+export class ChatDemo extends Chat {
+    private client: AxiosInstance;
+
+    constructor(options?: ChatOptions) {
+        super(options);
+        this.client = CreateAxiosProxy({
+            baseURL: 'https://chat.chatgptdemo.net',
+            headers: {
+                'Content-Type': 'application/json',
+                "accept": "text/event-stream",
+                "Cache-Control": "no-cache",
+                "Proxy-Connection": "keep-alive"
+            }
+        } as CreateAxiosDefaults);
+    }
+
+    support(model: ModelType): number {
+        switch (model) {
+            case ModelType.GPT3p5Turbo:
+                return 2000;
+            default:
+                return 0;
+        }
+    }
+
+    public async ask(req: ChatRequest): Promise<ChatResponse> {
+        const stream = new EventStream();
+        const res = await this.askStream(req, stream);
+        const result: ChatResponse = {
+            content: '',
+        }
+        return new Promise(resolve => {
+            stream.read((event, data) => {
+                switch (event) {
+                    case Event.done:
+                        break;
+                    case Event.message:
+                        result.content += (data as MessageData).content
+                        break;
+                    case Event.error:
+                        result.error = (data as ErrorData).error
+                        break;
+                }
+            }, () => {
+                resolve(result);
+            })
+        })
+
+    }
+
+    public async askStream(req: ChatRequest, stream: EventStream) {
+        const data: RealReq = {
+            question: req.prompt,
+            chat_id: v4(),
+            timestamp: moment().valueOf(),
+        };
+        try {
+            const res = await this.client.post('/chat_api_stream', data, {
+                responseType: 'stream',
+            } as AxiosRequestConfig);
+            res.data.pipe(es.split(/\r?\n\r?\n/)).pipe(es.map(async (chunk: any, cb: any) => {
+                const dataStr = chunk.replace('data: ', '');
+                if (!dataStr) {
+                    stream.end();
+                    return;
+                }
+                const data = parseJSON(dataStr, {} as any);
+                if (!data?.choices) {
+                    stream.write(Event.error, {error: 'not found data.choices'})
+                    stream.end();
+                    return;
+                }
+                const [{delta: {content = ""}}] = data.choices;
+                stream.write(Event.message, {content});
+            }))
+        } catch (e: any) {
+            console.error(e);
+            stream.write(Event.error, {error: e.message})
+            stream.end();
+        }
+    }
+}
--- model/index.ts
+++ model/index.ts
@@ -2,12 +2,14 @@ import {Chat, ChatOptions} from "./base";
 import {You} from "./you";
 import {Forefrontnew} from "./forefront";
 import {Mcbbs} from "./mcbbs";
+import {ChatDemo} from "./chatdemo";
 
 export enum Site {
     // define new model here
     You = 'you',
     Forefront = 'forefront',
     Mcbbs = 'mcbbs',
+    ChatDemo = 'chatdemo',
 }
 
 export class ChatModelFactory {
@@ -25,6 +27,7 @@ export class ChatModelFactory {
         this.modelMap.set(Site.You, new You(this.options))
         this.modelMap.set(Site.Forefront, new Forefrontnew(this.options))
         this.modelMap.set(Site.Mcbbs, new Mcbbs(this.options))
+        this.modelMap.set(Site.ChatDemo, new ChatDemo(this.options))
     }
 
     get(model: Site): Chat | undefined {
