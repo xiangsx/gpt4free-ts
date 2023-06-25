d6aa3067260e13cda08cfb10f3ceb624ea187fe1
xiang <1984871009@qq.com>
2023-06-25 09:35:03 +0800

    feat: 增加相似度检测

--- utils/index.ts
+++ utils/index.ts
@@ -5,6 +5,7 @@ import {v4} from "uuid";
 import {encoding_for_model} from '@dqbd/tiktoken'
 import TurndownService from "turndown";
 const turndownService = new TurndownService({codeBlockStyle:'fenced'});
+import stringSimilarity from 'string-similarity';
 
 const en = encoding_for_model("gpt-3.5-turbo");
 
@@ -136,4 +137,10 @@ export const getTokenSize = (str: string) => {
 
 export const htmlToMarkdown = (html:string):string=>{
     return turndownService.turndown(html);
+}
+
+export const isSimilarity=(s1:string,s2:string):boolean=>{
+    const similarity = stringSimilarity.compareTwoStrings(s1, s2);
+    console.log(similarity);
+    return similarity > 0.3;
 }
