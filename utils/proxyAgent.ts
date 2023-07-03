17a32514e11c19f9a5b076eff480dcb79b47a4d4
xiang <1984871009@qq.com>
2023-07-03 14:10:35 +0800

    feat: add cf 代理

--- utils/proxyAgent.ts
+++ utils/proxyAgent.ts
@@ -12,7 +12,25 @@ export function CreateAxiosProxy(config: CreateAxiosDefaults, proxy?: string): A
         createConfig.httpAgent = HttpsProxyAgent(useProxy);
         createConfig.httpsAgent = HttpsProxyAgent(useProxy);
     }
-    return axios.create(createConfig);
+    const client = axios.create(createConfig);
+    if (process.env.REQ_PROXY) {
+        client.interceptors.request.use(
+            (config) => {
+                config.params = {
+                    ...config.params,
+                    target: (config.baseURL || "") + (config.url || "")
+                }
+                config.baseURL = '';
+                config.url = process.env.REQ_PROXY || '';
+                return config;
+            },
+            error => {
+                // 对请求错误做些什么
+                return Promise.reject(error);
+            }
+        );
+    }
+    return client;
 }
 
 export function CreateTlsProxy(config: SessionConstructorOptions, proxy?: string): Session {
