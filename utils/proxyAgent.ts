e6f8f4bab95a9c05be1ba4b7762a268b6e953911
xiang <1984871009@qq.com>
2023-07-03 20:15:07 +0800

    feat: 增加错误重试

--- utils/proxyAgent.ts
+++ utils/proxyAgent.ts
@@ -4,6 +4,16 @@ import {SessionConstructorOptions} from "tls-client/dist/esm/types";
 import {Session} from "tls-client/dist/esm/sessions";
 import tlsClient from "tls-client";
 
+const reqProxy = (config: any) => {
+    config.params = {
+        ...config.params,
+        target: (config.baseURL || "") + (config.url || "")
+    }
+    config.baseURL = '';
+    config.url = process.env.REQ_PROXY || '';
+    return config;
+}
+
 export function CreateAxiosProxy(config: CreateAxiosDefaults, proxy?: string): AxiosInstance {
     const createConfig = {...config};
     const useProxy = process.env.http_proxy || proxy;
@@ -13,6 +23,7 @@ export function CreateAxiosProxy(config: CreateAxiosDefaults, proxy?: string): A
         createConfig.httpsAgent = HttpsProxyAgent(useProxy);
     }
     const client = axios.create(createConfig);
+    const retryClient = axios.create(createConfig);
     if (process.env.REQ_PROXY) {
         client.interceptors.request.use(
             (config) => {
@@ -29,6 +40,19 @@ export function CreateAxiosProxy(config: CreateAxiosDefaults, proxy?: string): A
                 return Promise.reject(error);
             }
         );
+    }
+    if (process.env.RETRY === "1") {
+        client.interceptors.response.use(undefined, function axiosRetryInterceptor(err) {
+            // 如果请求失败并且重试次数少于一次，则重试
+            if (err) {
+                // 返回 axios 实例，进行一次新的请求
+                console.log('axios failed, retrying!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
+                return retryClient(err.config);
+            }
+
+            // 如果失败且重试达到最大次数，将错误返回到用户
+            return Promise.reject(err);
+        });
     }
     return client;
 }
