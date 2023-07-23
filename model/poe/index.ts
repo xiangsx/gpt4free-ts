75f12798868aa479dcf9a48a0e1ba0ef7fc0ec77
xiang <1984871009@qq.com>
2023-07-23 19:54:43 +0800

    fix: 32k

--- model/poe/index.ts
+++ model/poe/index.ts
@@ -304,7 +304,7 @@ export class Poe extends Chat implements BrowserUser<Account> {
     public static TalkToGpt = "body > #__next > .LoggedOutBotInfoPage_layout__Y_z0i > .LoggedOutBotInfoPage_botInfo__r2z3X > .LoggedOutBotInfoPage_appButton__UO6NU";
 
     public async askStream(req: ChatRequest, stream: EventStream) {
-        req.prompt = req.prompt.replace(/assistant/g, 'answer');
+        req.prompt = req.prompt.replace(/assistant/g, 'result');
         const [page, account, done,
             destroy] = this.pagePool.get();
         let url = page?.url();
