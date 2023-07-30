1bf80ea21e33f8d5de038a5a74609d1e0949b35c
xiang <1984871009@qq.com>
2023-07-30 22:07:31 +0800

    fix: 修改权重

--- model/auto.ts
+++ model/auto.ts
@@ -21,9 +21,9 @@ const siteMap: ModelSiteMap = new Map([
     ],
     [ModelType.GPT3p5Turbo, [
         {site: Site.Bai, priority: 50},
+        {site: Site.Copilot, priority: 50},
+        {site: Site.PWeb, priority: 30},
         {site: Site.Chur, priority: 10},
-        {site: Site.Copilot, priority: 20},
-        {site: Site.PWeb, priority: 10},
         {site: Site.Poe, priority: 1},
         {site: Site.Cursor, priority: 10},
     ],
