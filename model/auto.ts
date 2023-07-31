bf30115d762c5486a907c754377d0a25bdafaa17
xiang <1984871009@qq.com>
2023-07-31 11:44:04 +0800

    feat: ram

--- model/auto.ts
+++ model/auto.ts
@@ -31,8 +31,8 @@ const siteMap: ModelSiteMap = new Map([
     ],
     [ModelType.GPT3p5_16k, [
         {site: Site.Chur, priority: 0},
-        {site: Site.OpenPrompt, priority: 60},
-        {site: Site.Poe, priority: 20},
+        {site: Site.OpenPrompt, priority: 0},
+        {site: Site.Poe, priority: 40},
     ]],
 ])
 
