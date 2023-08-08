a6d2a8c4a23136b828bd398aceb158d8faf8c27b
xiang <1984871009@qq.com>
2023-08-08 14:30:57 +0800

    fix: 去除config.json日志

--- utils/config.ts
+++ utils/config.ts
@@ -1,65 +1,67 @@
-import {existsSync, readFileSync, watch} from 'fs';
-import {ModelType, Site} from "../model/base";
+import { existsSync, readFileSync, watch } from 'fs';
+import { ModelType, Site } from '../model/base';
 
 // 首先定义配置的数据类型
 interface ConfigData {
-    site_map: Partial<Record<ModelType, { site: Site; priority: number; }[]>>;
-    // 当添加新字段时，需要在此处更新类型定义
+  site_map: Partial<Record<ModelType, { site: Site; priority: number }[]>>;
+  // 当添加新字段时，需要在此处更新类型定义
 }
 
 class BaseConfig {
-    private filePath: string = './run/config.json';
-    private defaultConfig: ConfigData = {
-        site_map: {},
-        // Add new fields here, with their default values
-    };
-    public config: ConfigData;
+  private filePath: string = './run/config.json';
+  private defaultConfig: ConfigData = {
+    site_map: {},
+    // Add new fields here, with their default values
+  };
+  public config: ConfigData;
 
-    constructor() {
-        // Initialize config as a deep copy of defaultConfig
-        this.config = JSON.parse(JSON.stringify(this.defaultConfig));
+  constructor() {
+    // Initialize config as a deep copy of defaultConfig
+    this.config = JSON.parse(JSON.stringify(this.defaultConfig));
+  }
+
+  load() {
+    if (!existsSync(this.filePath)) {
+      console.log(
+        `Configuration file ${this.filePath} not found. Retrying in 5 seconds...`,
+      );
+      setTimeout(() => this.load(), 5000);
+      return;
     }
+    try {
+      const rawData = readFileSync(this.filePath, 'utf8');
+      const fileConfig: Partial<ConfigData> = JSON.parse(rawData);
 
-    load() {
-        if (!existsSync(this.filePath)) {
-            console.log(`Configuration file ${this.filePath} not found. Retrying in 5 seconds...`);
-            setTimeout(() => this.load(), 5000);
-            return;
-        }
-        try {
-            const rawData = readFileSync(this.filePath, 'utf8');
-            const fileConfig: Partial<ConfigData> = JSON.parse(rawData);
-
-            // Merge defaultConfig and fileConfig
-            this.config = Object.assign(this.config, this.defaultConfig, fileConfig);
-            console.log('Loaded config from run/config.json successfully!', JSON.stringify(this.config));
-        } catch (error) {
-            console.error(`Error reading or parsing the configuration file ${this.filePath}.`, error);
-        }
+      // Merge defaultConfig and fileConfig
+      this.config = Object.assign(this.config, this.defaultConfig, fileConfig);
+      // console.log('Loaded config from run/config.json successfully!', JSON.stringify(this.config));
+    } catch (error) {
+      // console.error(`Error reading or parsing the configuration file ${this.filePath}.`, error);
     }
+  }
 
-    watchFile() {
-        if (!existsSync(this.filePath)) {
-            // console.log(`Configuration file ${this.filePath} not found. Retrying in 5 seconds...`);
-            setTimeout(() => this.watchFile(), 5000);
-            return;
-        }
-        let timeoutId: NodeJS.Timeout | null = null;
-        const debounceDelay = 300;
-
-        watch(this.filePath, (event) => {
-            if (event === 'change') {
-                if (timeoutId) {
-                    clearTimeout(timeoutId);
-                }
-
-                timeoutId = setTimeout(() => {
-                    // console.log(`Configuration file ${this.filePath} has been changed! Reloading...`);
-                    this.load();
-                }, debounceDelay);
-            }
-        });
+  watchFile() {
+    if (!existsSync(this.filePath)) {
+      // console.log(`Configuration file ${this.filePath} not found. Retrying in 5 seconds...`);
+      setTimeout(() => this.watchFile(), 5000);
+      return;
     }
+    let timeoutId: NodeJS.Timeout | null = null;
+    const debounceDelay = 300;
+
+    watch(this.filePath, (event) => {
+      if (event === 'change') {
+        if (timeoutId) {
+          clearTimeout(timeoutId);
+        }
+
+        timeoutId = setTimeout(() => {
+          // console.log(`Configuration file ${this.filePath} has been changed! Reloading...`);
+          this.load();
+        }, debounceDelay);
+      }
+    });
+  }
 }
 
 export const Config = new BaseConfig();
