8999f773dca20ab3cba96937d0642abb153de420
xiang <1984871009@qq.com>
2023-08-18 21:27:56 +0800

    feat: poef

--- model/poef/index.ts
+++ model/poef/index.ts
@@ -398,7 +398,9 @@ export class Poef extends Chat implements BrowserUser<Account> {
         '.LoggedOutSection_main__QtksL > .LoggedOutSection_appSpecificSection__C5YEM > .MainSignupLoginSection_inputAndMetaTextGroup__5ITsJ > .EmailInput_wrapper__D9Dss > .EmailInput_emailInput__4v_bn',
         { timeout: 60 * 1000 },
       );
-      const emailBox = CreateEmail(TempEmailType.SmailPro);
+      const emailBox = CreateEmail(
+        (process.env.POEF_MAIL_TYPE as TempEmailType) || TempEmailType.Gmail,
+      );
       const emailAddress = await emailBox.getMailAddress();
       // const emailAddress = 'backs-walkout-0o@icloud.com';
       // 输入邮箱
