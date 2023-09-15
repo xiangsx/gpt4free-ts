b54081485ba3e65a9c01dc509e3421737836322f
xiang <1984871009@qq.com>
2023-09-15 20:05:48 +0800

    fix(poe): 统一10秒延迟

--- model/poe/index.ts
+++ model/poe/index.ts
@@ -492,35 +492,32 @@ ${question}`;
     try {
       let old = '';
       let et: EventEmitter;
-      const tt = setTimeout(
-        async () => {
-          try {
-            client.removeAllListeners('Network.webSocketFrameReceived');
-            stream.write(Event.error, { error: 'please retry later!' });
-            stream.write(Event.done, { content: '' });
-            stream.end();
-            await Poe.clearContext(page);
-            await page.reload();
-            this.logger.info(
-              `poe time out, return error! failed prompt: [\n${req.prompt}\n]`,
-            );
-            account.failedCnt += 1;
-            this.accountPool.syncfile();
-            if (account.failedCnt >= MaxFailedTimes) {
-              destroy();
-              this.accountPool.syncfile();
-              this.logger.info(`poe account failed cnt > 10, destroy ok`);
-            } else {
-              await page.reload();
-              done(account);
-            }
-          } catch (e) {
+      const tt = setTimeout(async () => {
+        try {
+          client.removeAllListeners('Network.webSocketFrameReceived');
+          stream.write(Event.error, { error: 'please retry later!' });
+          stream.write(Event.done, { content: '' });
+          stream.end();
+          await Poe.clearContext(page);
+          await page.reload();
+          this.logger.info(
+            `poe time out, return error! failed prompt: [\n${req.prompt}\n]`,
+          );
+          account.failedCnt += 1;
+          this.accountPool.syncfile();
+          if (account.failedCnt >= MaxFailedTimes) {
             destroy();
-            this.logger.error(`err in timeout: `, e);
+            this.accountPool.syncfile();
+            this.logger.info(`poe account failed cnt > 10, destroy ok`);
+          } else {
+            await page.reload();
+            done(account);
           }
-        },
-        req.model === ModelType.Claude2_100k ? 60 * 1000 : 10 * 1000,
-      );
+        } catch (e) {
+          destroy();
+          this.logger.error(`err in timeout: `, e);
+        }
+      }, 10 * 1000);
       let currMsgID = '';
       et = client.on('Network.webSocketFrameReceived', async ({ response }) => {
         try {
