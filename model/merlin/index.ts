5e07bb593ab1159fe889dbf5e804b9e5cb8024a3
xiang <1984871009@qq.com>
2023-09-17 22:07:55 +0800

    feat(merlin): 加大超时时间

--- model/merlin/index.ts
+++ model/merlin/index.ts
@@ -201,7 +201,7 @@ export class Merlin extends Chat {
       }
       return true;
     },
-    { delay: 1000 },
+    { delay: 10000 },
   );
   constructor(options?: ChatOptions) {
     super(options);
