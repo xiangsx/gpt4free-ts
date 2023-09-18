15de41b5fa9854c11d93cf3294414df6e7991ae5
xiang <1984871009@qq.com>
2023-09-18 10:24:17 +0800

    fix(ddg): 搜索关键词长度问题

--- model/ddg/index.ts
+++ model/ddg/index.ts
@@ -42,7 +42,10 @@ export class DDG extends Chat {
     const page = await this.newPage();
     try {
       await page.goto(
-        `https://duckduckgo.com/?kad=zh_CN&kk=-1&k1=-1&kau=-1&kao=-1&kap=-1&kaq=-1&kax=-1&kak=-1&kv=-1&kp=1&kah=cn-zh&kl=cn-zh&q=${req.prompt}`,
+        `https://duckduckgo.com/?kad=zh_CN&kk=-1&k1=-1&kau=-1&kao=-1&kap=-1&kaq=-1&kax=-1&kak=-1&kv=-1&kp=1&kah=cn-zh&kl=cn-zh&q=${req.prompt.slice(
+          0,
+          500,
+        )}`,
         {
           waitUntil: 'domcontentloaded',
         },
