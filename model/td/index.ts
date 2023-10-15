445ce58444b9b26cec7a859df1923ad93c3ca56b
xiang <1984871009@qq.com>
2023-10-15 21:43:19 +0800

    fix(td): 失败destory

--- model/td/index.ts
+++ model/td/index.ts
@@ -120,7 +120,7 @@ class Child extends ComChild<Account> {
     );
     if (!res.data.token) {
       this.destroy({ delFile: false, delMem: true });
-      throw new Error('refresh auth failed');、
+      throw new Error('refresh auth failed');
     }
     this.update({ token: res.data.token });
     this.logger.info('refresh auth ok');
