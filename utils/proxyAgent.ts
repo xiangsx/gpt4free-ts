8d8d47e70b0cb55e9e206c24d405f96502f2d284
xiang <1984871009@qq.com>
2023-10-17 09:51:20 +0800

    fix(puppeteer): 创建失败自动关闭浏览器

--- utils/proxyAgent.ts
+++ utils/proxyAgent.ts
@@ -133,16 +133,22 @@ export async function CreateNewPage(
     launchOpt.args?.push(`--proxy-server=${proxy}`);
   }
   const browser = await puppeteer.launch(launchOpt);
-  const page = await browser.newPage();
-  if (simplify) {
-    await simplifyPage(page);
+  try {
+    const page = await browser.newPage();
+    if (simplify) {
+      await simplifyPage(page);
+    }
+    if (cookies.length > 0) {
+      await page.setCookie(...cookies);
+    }
+    await page.setViewport({ width: 1920, height: 1080 });
+    await page.goto(url);
+    return page;
+  } catch (e) {
+    console.error(e);
+    await browser.close();
+    throw e;
   }
-  if (cookies.length > 0) {
-    await page.setCookie(...cookies);
-  }
-  await page.setViewport({ width: 1920, height: 1080 });
-  await page.goto(url);
-  return page;
 }
 
 export async function CreateNewPageWithWS(
