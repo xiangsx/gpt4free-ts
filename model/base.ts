c4ca916acd298134a08882ff8e9569773c3099fb
xiang <1984871009@qq.com>
2023-10-20 14:36:48 +0800

    feat(base): 高效处理过长对话

--- model/base.ts
+++ model/base.ts
@@ -198,7 +198,7 @@ export function sliceMessagesByLength(
     return messages;
   }
   const newMessage =
-    size / limitSize > 2
+    size / limitSize > 2 && messages.length > 21
       ? messages.slice(-20)
       : messages.slice(1, messages.length);
   if (newMessage.length === 0) {
