28d93aa27438d28a074cd58b34171db993ff2856
xiang <1984871009@qq.com>
2023-11-22 14:41:56 +0800

    feat: 异常

--- index.ts
+++ index.ts
@@ -31,9 +31,9 @@ if (cluster.isPrimary) {
 }
 process.on('uncaughtException', (err) => {
   console.error('Uncaught exception:', err);
-  // process.exit(1); // It's up to you whether to exit here or not
+  process.exit(1); // It's up to you whether to exit here or not
 });
 process.on('unhandledRejection', (reason, promise) => {
   console.error('Unhandled rejection at ', promise, `reason: ${reason}`);
-  // process.exit(1); // It's up to you whether to exit here or not
+  process.exit(1); // It's up to you whether to exit here or not
 });
