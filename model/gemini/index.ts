780b29c95509720ea328886119a381090ac0c252
xiangsx <1984871009@qq.com>
2023-12-15 10:41:48 +0800

    feat: 优化gemini

--- model/gemini/index.ts
+++ model/gemini/index.ts
@@ -132,23 +132,15 @@ class Child extends ComChild<Account> {
     const data = {
       safetySettings: [
         {
-          category: HarmCategory.HARM_CATEGORY_HARASSMENT,
-          threshold: HarmBlockThreshold.BLOCK_NONE,
-        },
-        {
-          category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
-          threshold: HarmBlockThreshold.BLOCK_NONE,
-        },
-        {
-          category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
-          threshold: HarmBlockThreshold.BLOCK_NONE,
-        },
-        {
           category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
           threshold: HarmBlockThreshold.BLOCK_NONE,
         },
         {
-          category: HarmCategory.HARM_CATEGORY_UNSPECIFIED,
+          category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+          threshold: HarmBlockThreshold.BLOCK_NONE,
+        },
+        {
+          category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
           threshold: HarmBlockThreshold.BLOCK_NONE,
         },
       ],
