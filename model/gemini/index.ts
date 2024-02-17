724cd34bf682df596728f2c507ca699eb3922f46
xiangsx <1984871009@qq.com>
2024-02-17 16:40:15 +0800

    feat: gemini处理

--- model/gemini/index.ts
+++ model/gemini/index.ts
@@ -35,8 +35,6 @@ interface Account extends ComInfo {
   apikey: string;
 }
 
-const EndChars = '|END|';
-
 class Child extends ComChild<Account> {
   client: AxiosInstance = CreateNewAxios(
     {
@@ -202,12 +200,6 @@ class Child extends ComChild<Account> {
       if (hasImage) {
         data.contents.push(...content);
         data.contents = await this.preHandleContent(data.contents);
-        for (const v of data.contents[data.contents.length - 1].parts) {
-          if (v.text) {
-            v.text = v.text += `
-Tips: Your answer must end with ${EndChars}`;
-          }
-        }
         return this.client.post(
           `/v1beta/models/${model}:streamGenerateContent?key=${this.info.apikey}&alt=sse`,
           data,
@@ -224,12 +216,6 @@ Tips: Your answer must end with ${EndChars}`;
       data.contents.push(...content);
     }
     data.contents = await this.preHandleContent(data.contents);
-    for (const v of data.contents[data.contents.length - 1].parts) {
-      if (v.text) {
-        v.text = v.text += `
-Tips: If the answer is finished, please end with ${EndChars}`;
-      }
-    }
     return this.client.post(
       `/v1beta/models/${targetModel}:streamGenerateContent?key=${this.info.apikey}&alt=sse`,
       data,
@@ -328,22 +314,11 @@ export class Gemini extends Chat {
       const delay = setTimeout(() => {
         stream.write(Event.done, { content: '' });
         stream.end();
-        this.logger.info('Recv msg ok, End timeout');
       }, 5000);
       response.on('data', (content: string) => {
         try {
           delay.refresh();
-          if (content.indexOf(EndChars) > -1) {
-            stream.write(Event.message, {
-              content: content.replace(EndChars, ''),
-            });
-            stream.write(Event.done, { content: '' });
-            stream.end();
-            clearTimeout(delay);
-            this.logger.info('Recv msg ok, End ok');
-          } else {
-            stream.write(Event.message, { content: content });
-          }
+          stream.write(Event.message, { content: content });
         } catch (e: any) {
           this.logger.error(e.message);
         }
@@ -351,9 +326,7 @@ export class Gemini extends Chat {
       response.on('error', this.logger.error);
     } catch (e: any) {
       e.response.data.on('data', (chunk: any) =>
-        this.logger.error(
-          `req:${JSON.stringify(req.messages)} err:${chunk.toString()}`,
-        ),
+        this.logger.error(chunk.toString()),
       );
       console.error(e.message);
       throw new ComError(e.message, ComError.Status.InternalServerError);
