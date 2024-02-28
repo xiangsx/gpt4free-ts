729da42fc73cbeb8411efa89aaac229b9aeba02d
xiangsx <1984871009@qq.com>
2024-02-28 10:39:38 +0800

    feat: 狗头消失

--- model/midjourney/child.ts
+++ model/midjourney/child.ts
@@ -64,7 +64,7 @@ export class Child extends DiscordChild<Account> {
     if (mCreate.d.embeds?.length) {
       const error = `### ${mCreate.d.embeds?.[0].title}\n\n ${mCreate.d.embeds?.[0].description}`;
       onError(new Error(error));
-      if (error.includes('Blocked')) {
+      if (error.toLowerCase().includes('blocked')) {
         this.update({ blocked: true });
         this.destroy({ delFile: false, delMem: true });
       }
