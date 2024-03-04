f8ee20eb64ea2135fb9533fe549bde5e87518005
xiangsx <1984871009@qq.com>
2024-03-04 23:09:07 +0800

    feat: 支持claude3

--- model/claudeapi/index.ts
+++ model/claudeapi/index.ts
@@ -3,6 +3,7 @@ import { AxiosInstance, AxiosRequestConfig, CreateAxiosDefaults } from 'axios';
 import { CreateAxiosProxy } from '../../utils/proxyAgent';
 import es from 'event-stream';
 import { ComError, Event, EventStream, parseJSON } from '../../utils';
+import { AsyncStoreSN } from '../../asyncstore';
 
 interface Message {
   role: string;
@@ -21,12 +22,48 @@ interface RealReq {
   stream?: boolean;
 }
 
+interface MessagesReq extends ChatRequest {
+  functions?: {
+    name: string;
+    description?: string;
+    parameters: object;
+  };
+  function_call?: string;
+  temperature?: number;
+  top_p?: number;
+  n?: number;
+  stream?: boolean;
+  stop?: string | string[];
+  max_tokens?: number;
+  presence_penalty?: number;
+  frequency_penalty?: number;
+  logit_bias?: {};
+  user?: string;
+}
+
 interface ClaudeChatOptions extends ChatOptions {
   base_url?: string;
   api_key?: string;
   proxy?: boolean;
   model_map?: { [key: string]: ModelType };
 }
+
+const MessagesParamsList = [
+  'model',
+  'messages',
+  'functions',
+  'function_call',
+  'temperature',
+  'top_p',
+  'n',
+  'stream',
+  'stop',
+  'max_tokens',
+  'presence_penalty',
+  'frequency_penalty',
+  'logit_bias',
+  'user',
+];
 
 const ParamsList = [
   'model',
@@ -94,7 +131,82 @@ export class ClaudeAPI extends Chat {
     return reqH;
   }
 
+  async askMessagesStream(req: MessagesReq, stream: EventStream) {
+    const data: MessagesReq = {
+      ...req,
+      messages: req.messages,
+      model: req.model,
+      stream: true,
+      max_tokens: req.max_tokens || 4000,
+    };
+    for (const key in data) {
+      if (MessagesParamsList.indexOf(key) === -1) {
+        delete (data as any)[key];
+      }
+    }
+    try {
+      const res = await this.client.post('/v1/messages', data, {
+        responseType: 'stream',
+        headers: {
+          'x-api-key': `${this.options?.api_key || req.secret || ''}`,
+          'x-request-id': AsyncStoreSN.getStore()?.sn,
+        },
+      } as AxiosRequestConfig);
+      res.data.pipe(es.split(/\r?\n\r?\n/)).pipe(
+        es.map(async (chunk: any, cb: any) => {
+          const dataStr = chunk.split('\n')[1]?.replace('data: ', '');
+          if (!dataStr) {
+            return;
+          }
+          if (dataStr === '[DONE]') {
+            return;
+          }
+          const data = parseJSON<{
+            content_block: { text: string };
+            delta: { text: string };
+            type:
+              | 'message_stop'
+              | 'message_delta'
+              | 'content_block_stop'
+              | 'content_block_start';
+          }>(dataStr, {} as any);
+          if (data.delta) {
+            stream.write(Event.message, { content: data.delta.text });
+            return;
+          }
+          if (data.content_block) {
+            stream.write(Event.message, { content: data.content_block.text });
+            return;
+          }
+        }),
+      );
+      res.data.on('close', () => {
+        stream.write(Event.done, { content: '' });
+        stream.end();
+      });
+    } catch (e: any) {
+      if (e.response && e.response.data) {
+        e.message = await new Promise((resolve, reject) => {
+          e.response.data.on('data', (chunk: any) => {
+            const content = chunk.toString();
+            this.logger.error(content);
+            resolve(
+              parseJSON<{ error?: { message?: string } }>(content, {})?.error
+                ?.message || content,
+            );
+          });
+        });
+      }
+      this.logger.error(`claude messages failed: ${e.message}`);
+      stream.write(Event.error, { error: e.message, status: e.status });
+      stream.end();
+    }
+  }
+
   public async askStream(req: ChatRequest, stream: EventStream) {
+    if (req.model.startsWith('claude-3')) {
+      return this.askMessagesStream(req as MessagesReq, stream);
+    }
     const data: RealReq = {
       max_tokens_to_sample: 100 * 10000,
       ...req,
