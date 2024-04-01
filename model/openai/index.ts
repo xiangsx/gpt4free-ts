d064ab51b0952b7fc62763f6f6358add962f6f18
xiangsx <1984871009@qq.com>
2024-04-01 18:55:52 +0800

    feat: 去除日志

--- model/openai/index.ts
+++ model/openai/index.ts
@@ -168,7 +168,6 @@ export class OpenAI extends Chat {
         e.message = await new Promise((resolve, reject) => {
           e.response.data.on('data', (chunk: any) => {
             const content = chunk.toString();
-            this.logger.error(content);
             resolve(
               parseJSON<{ error?: { message?: string } }>(content, {})?.error
                 ?.message || content,
